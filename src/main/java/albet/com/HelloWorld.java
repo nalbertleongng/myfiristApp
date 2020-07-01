cd67da524b85feeb9b12047af629ac056bfc8f13
AlbertLeong <ngcasio22@gmail.com>
2020-07-01 14:58:28 +0800

    Java and Kotlin variable, data type

--- src/main/java/albet/com/HelloWorld.java
+++ src/main/java/albet/com/HelloWorld.java
@@ -1,12 +1,11 @@
 package albet.com;
 
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        System.out.println("Hello world");
 //        System.out.println("AlbertLeong");
         Person p = new Person();
         p.hello();
+        int age = 19;
     }
-
-
 }
--- src/main/kotlin/albet/com/HelloWorld.kt
+++ src/main/kotlin/albet/com/HelloWorld.kt
@@ -5,6 +5,21 @@ fun main() {
 //    Human().hello()
 val h = Human()
     h.hello()
+    Int
+    var age =19;
+    val age2 =19;
+    age = 20;
+
+    //var 能改
+    //val 不能改
+    //變數型態自動推斷 不用定義也可以
+    val name = "Albert";
+    var name2: String;
+    name2 = "Hank";
+    //;可有可冇
+
+
+
 }
 
 class Human{
