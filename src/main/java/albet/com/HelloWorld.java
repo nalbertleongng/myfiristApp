1848184454e38d5d4013aadb388deae4fc5383b2
AlbertLeong <ngcasio22@gmail.com>
2020-07-01 14:02:13 +0800

    Initial commit

---
+++ src/main/java/albet/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package albet.com;
+
+public class HelloWorld {
+    public static void main(String[] args){
+//        System.out.println("Hello world");
+//        System.out.println("AlbertLeong");
+        Person p = new Person();
+        p.hello();
+    }
+
+
+}
---
+++ src/main/java/albet/com/Person.java
@@ -0,0 +1,7 @@
+package albet.com;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
